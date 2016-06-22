1018a3779863e8a7bbf2c9e4406e775ab1c71595
bruno <bruno@dump.net.br>
2016-06-22 04:19:45 -0300

    Inserção da funcinalidade após processar dados

--- src/main/webapp/js/controllers/scdCtrl.js
+++ src/main/webapp/js/controllers/scdCtrl.js
@@ -109,26 +109,85 @@ angular.module("scd").controller("scdCtrl", function ($scope, $http) {
             delete regra;
         }        
     }
+
+    $scope.visualizarCondicoesFuzzificadas = function(variaveis) {
+        $scope.regras = [],
+        variaveisFuzzificadas = [],
+        condicoes = [],
+        variaveisAtivacao = [];
+
+        for (var i = 0; i < variaveis.length; i++) {
+            var variavel = variaveis[i],
+                variavelObjetivo = "",
+                valorConsequente = "";
+
+            for (var j = 0; j < variavel.termos.length; j++) {  
+
+                var regra = {},                            
+                    linha = {};
+
+                if(variavel.objetivo !== "Sim") {
+                    var variavelFuzzificado = variavel.termos[j];
+                    variaveisFuzzificadas.push(variavelFuzzificado);
+
+                } else {
+                    var variavelAtivacao = variavel.termos[j];
+                    variaveisAtivacao.push(variavelAtivacao);
+
+                    valorConsequente = variavel.termos[j].descricao;
+                    condicoes        = variavel.termos[j].condicoes;
+                    variavelObjetivo = variavel; 
+                }                 
+
+                if(condicoes.length > 0) {
+
+                    regra.linhas     = [];
+                    regra.descricao  = "Regra " + (j + 1);    
+
+                    for(var k = 0; k < condicoes.length; k++) {
+                        var v        = condicoes[k].variavel.descricao,
+                            termo    = condicoes[k].termo.descricao,                            
+                            valorFuzzy = variaveisFuzzificadas.filter(function( obj ) {
+                                return obj.descricao == termo;
+                            });
+
+                        if(k > 0 && k < condicoes.length) {
+                            linha.condicao = "E ";
+                        } else {
+                            linha.condicao = "SE ";
+                        }
+
+                        linha.valor = v + " = " + termo + " (" + valorFuzzy[0].valorFuzzificado + ")";
+
+                        regra.linhas.push(angular.copy(linha));
+                        delete linha;
+                    }    
+
+                    linha.condicao = "ENTÃO ";
+                    linha.valor =  variavelObjetivo.descricao + " = " + valorConsequente + " (" + variaveisAtivacao[j].valorAtivacao + ")";
+                    regra.linhas.push(angular.copy(linha));
+                    delete linha;
+
+                    $scope.regras.push(angular.copy(regra));
+                    delete regra;     
+                }                 
+            }    
+        } 
+    }    
     
     $scope.adicionarCondicao = function() {
         $scope.termoObjetivo.condicoes.push({});
     }
 
     $scope.processar = function(variaveis) {
-        // $http.post("/sistema-controle-difuso/rest/algoritmoREST", variaveis).success(function(data) {
-        //     console.log(data);
-
+        // $http.get("/sistema-controle-difuso/rest/algoritmoREST", variaveis).success(function(data) {
+        //     $scope.resultado = json;         
+        //     $scope.visualizarCondicoesFuzzificadas($scope.resultado);
         // });
 
-        $.ajax({
-            dataType: "json",
-            url: "teste.json",
-            mimeType: "application/json",
-            success: function(json) {                
-                $scope.resultado = json;
-                console.table($scope.resultado);
-            }
-        });        
+        $http.get("teste.json").success(function(json) {
+                $scope.resultado = json;         
+                $scope.visualizarCondicoesFuzzificadas($scope.resultado);
+        });
     }
-    
 });
